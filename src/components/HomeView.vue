a8190c3fbad401219d26f4e3b460907a8da834cf
lzwjava <lzwjava@gmail.com>
2015-12-09 20:57:55 +0800

    finish card sample

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -46,7 +46,7 @@
             </div>
 
             <p class="detail">
-              每一次Code Review的过程都凝结着大神的心血Code Review上线半年以来，已经帮助重构、简化了1400份代码文件。
+              每一次 Code Review 的过程都凝结着大神的心血 Code Review 上线半年以来，已经帮助重构、简化了 1400 份代码文件。
             </p>
           </div>  
 
@@ -58,7 +58,7 @@
             </div>
 
             <p class="detail">
-              代码审核无论对于审核者还是帮助申请人来说，都是宝贵的经验财富，在获得许可的情况下，我们提供Code Review经验文摘。
+              代码审核无论对于审核者还是帮助申请人来说，都是宝贵的经验财富，在获得许可的情况下，我们提供 Code Review 经验文摘。
             </p>
           </div> 
         </div>
@@ -68,6 +68,33 @@
     </section>
 
     <section class="section3">
+
+      <div class="intro">
+
+        <div class="row">
+          <h2>发现行业内精英开发者</h2>
+          <h3>一览各个语言领域顶尖的编程人物</h3>
+        </div>
+
+        <div class="reviewers">
+          <ul>
+            <li>
+              <div class="card-header">
+                  <img class="avatar" src="../img/avatar1.png" >
+                  <div class="info">
+                    <p class="name">叶孤城</p>
+                    <p>经验丰富的 iOS 开发者，曾就职于 360Camera、英语流利说</p>
+                  </div>
+              </div>
+              <div class="card-footer">
+                <p class="review-num">7</p>
+                <p>Review 案例</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+      </div>
 
     </section>
 
@@ -105,20 +132,22 @@ section
           font-size 20px
           border-radius 4px
 
+
+.intro .row
+    text-align center
+    padding-top 60px
+    h2, h3
+      font-weight 100
+
 .section2
   height 900px
   .intro
     height 500px
     background url("../img/section2_bg.png")
-    .row
-      text-align center
-      padding-top 60px
-      h2, h3
-        font-weight 100
   .features
     background #fff
     .wrapper
-      padding 50px 200px;
+      padding 50px 200px
       .feature 
         display inline-block
         width 33%
@@ -126,11 +155,42 @@ section
         line-height 20px        
         .short
           padding 10px 50px
-          p
-            font-size 16px
+        .detail
+          padding 0 10px
+          font-size 14px
 
 .section3
   height 850px
-  background #333
+  background rgb(244, 245, 247)
+  .reviewers
+    padding 100px 200px
+    ul
+      list-style none
+      li
+        background #fff
+        width 33%
+        text-align center
+        line-height 20px
+        .card-header
+          position relative
+          background #fff
+          height 150px
+          padding 5px 10px
+          img
+            position relative
+            top -80px
+          .info
+            position relative
+            top -85px
+            p.name
+              font-size 18px
+        .card-footer
+          position relative
+          background rgb(250, 250, 250)
+          padding-bottom 2px
+          p.review-num
+            font-size 24px
+            font-weight 500
+            padding-top 15px
 
 </style>
