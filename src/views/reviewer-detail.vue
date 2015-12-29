419978cd3ea4273754a888f7235cf763ccd26931
lzwjava <lzwjava@gmail.com>
2015-12-30 02:05:05 +0800

    加入申请review form

--- src/views/reviewer-detail.vue
+++ src/views/reviewer-detail.vue
@@ -29,13 +29,13 @@
                     </div>
 
                     <div>
-                        <button class="order-button">申请 Code Review</button>
+                        <button class="order-button" @click="showOrder = true">申请 Code Review</button>
                     </div>
                 </div>
             </div>
 
             <div class="right-region">
-                <p class="tag-title">擅长领域</p>
+                <p class="region-title">擅长领域</p>
                 <div class="tag-list">
                     <tag v-for="tag in reviewer.tags" :tag="tag"></tag>
                 </div>
@@ -47,6 +47,8 @@
                 审核案例
             </div>
         </div>
+
+        <order-form transition="fade" :show.sync="showOrder"></order-form>
     </div>
 
 </template>
@@ -54,8 +56,10 @@
 <script type="text/javascript">
 
 import UserAvatar from '../components/user-avatar.vue'
+import OrderForm from '../components/order-form.vue'
 import Tag from '../components/tag.vue'
 import util from '../common/util'
+
 import serviceUrl from "../common/serviceUrl.js"
 
 var debug = require('debug')('reviewer-detail');
@@ -63,19 +67,20 @@ var debug = require('debug')('reviewer-detail');
 export default {
     components: {
         'user-avatar': UserAvatar,
-        'tag': Tag
+        'tag': Tag,
+        'order-form': OrderForm
     },
     data () {
         return {
             reviewer: {
                 tags: []
-            }
+            },
+            showOrder: false
         }
     },
     methods: {
 
     },
-
     created() {
         var params = util.getSearchParameters()
         if (!params.id) {
@@ -123,8 +128,8 @@ export default {
     margin-top 30px
 
 .region-title
-    margin-left 50px
-    margin-top 50px 
+    margin-left 35px
+    margin-top 40px 
 
 .center-region
     width 373px
@@ -155,16 +160,12 @@ export default {
         margin-top 10px 
 
 .order-button
-    margin-top 20px
+    margin-top 50px
     background #00CFF5
     border-radius 3px
     font-size 16px
     color #FFFFFF
     line-height 16px
     padding 20px
-
-.tag-title
-    margin-left 25px
-    margin-top 25px
 
 </style>
